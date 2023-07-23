eccf2e0e3c64786ce70636d9ec69a15cf6bebf56
Nuno Pinheiro <npinheiro@letsgetchecked.com>
2023-07-23 04:22:05 +0100

    test ServiceApi: Add unit tests for command and query handlers

---
+++ tests/ServiceApi.UnitTests/CreateChatRecordCommandHandlerTests.cs
@@ -0,0 +1,60 @@
+﻿using ChatHistory.Domain.ChatRecords;
+using ChatHistory.ServiceApi.ChatRecords;
+using OneOf.Types;
+
+namespace ServiceApi.UnitTests;
+
+public class CreateChatRecordCommandHandlerTests
+{
+    private readonly Fixture Fixture = new();
+    private readonly Mock<IChatHistoryRepository> RepositoryMock = new(MockBehavior.Strict);
+    private readonly CreateChatRecordCommandHandler Handler;
+
+    public CreateChatRecordCommandHandlerTests()
+    {
+        Handler = new(new CreateChatRecordCommandValidator(), RepositoryMock.Object);
+    }
+
+    [Fact]
+    public async Task Handle_InvalidInputs_ReturnsValidationFailures()
+    {
+        // Arrange
+        var invalidCommand = Fixture.Create<CreateChatRecordCommand>();
+
+        // Act
+        var result = await Handler.Handle(invalidCommand);
+
+        // Assert
+        result.AsT1.Should().NotBeNullOrEmpty();
+
+        RepositoryMock.Verify(
+            r => r.AddChatHistoryEvent(It.IsAny<ChatRecordEvent>(), It.IsAny<CancellationToken>()),
+            Times.Never);
+    }
+
+    [Fact]
+    public async Task Handle_ValidInputs_CallsChatHistoryRepository()
+    {
+        // Arrange
+        CreateChatRecordCommand validCommand = new(
+            "comment",
+            "2023-07-17T00:00:00Z",
+            Fixture.Create<string>(),
+            Fixture.Create<string>(),
+            Fixture.Create<string>());
+
+        RepositoryMock
+            .Setup(r => r.AddChatHistoryEvent(It.IsAny<ChatRecordEvent>(), It.IsAny<CancellationToken>()))
+            .Returns(Task.CompletedTask);
+
+        // Act
+        var result = await Handler.Handle(validCommand);
+
+        // Assert
+        result.AsT0.Should().BeEquivalentTo(new Success());
+
+        RepositoryMock.Verify(
+            r => r.AddChatHistoryEvent(It.IsAny<ChatRecordEvent>(), It.IsAny<CancellationToken>()),
+            Times.Once);
+    }
+}
---
+++ tests/ServiceApi.UnitTests/ReadChatRecordsQueryHandlerTests.cs
@@ -0,0 +1,80 @@
+﻿using ChatHistory.Domain.ChatRecords;
+using ChatHistory.Domain.ValueObjects;
+using ChatHistory.ServiceApi.ChatRecords;
+using OneOf.Types;
+
+namespace ServiceApi.UnitTests;
+
+public class ReadChatRecordsQueryHandlerTests
+{
+    private readonly Fixture Fixture = new();
+    private readonly Mock<IChatHistoryRepository> RepositoryMock = new(MockBehavior.Strict);
+    private readonly ReadChatRecordsQueryHandler Handler;
+
+    public ReadChatRecordsQueryHandlerTests()
+    {
+        Handler = new(new ReadChatRecordsQueryValidator(), RepositoryMock.Object);
+    }
+
+    [Fact]
+    public async Task Handle_InvalidInputs_ReturnsValidationFailures()
+    {
+        // Arrange
+        var invalidCommand = Fixture.Create<ReadChatRecordsQuery>();
+
+        // Act
+        var result = await Handler.Handle(invalidCommand);
+
+        // Assert
+        result.AsT1.Should().NotBeNullOrEmpty();
+
+        RepositoryMock.Verify(
+            r => r.AddChatHistoryEvent(It.IsAny<ChatRecordEvent>(), It.IsAny<CancellationToken>()),
+            Times.Never);
+    }
+
+    [Theory]
+    [InlineData(Granularity.MinuteByMinute)]
+    [InlineData(Granularity.Hourly)]
+    [InlineData(Granularity.Daily)]
+    [InlineData(Granularity.Monthly)]
+    [InlineData(Granularity.Yearly)]
+    public async Task Handle_ValidInputs_CallsChatHistoryRepository(Granularity granularity)
+    {
+        // Arrange
+        ReadChatRecordsQuery validQuery = new(
+            granularity,
+            Fixture.Create<int>(),
+            Fixture.Create<int>(),
+            "2023-07-17T00:00:00Z",
+            "2023-07-17T00:00:00Z");
+
+        if (granularity is Granularity.MinuteByMinute)
+            RepositoryMock
+                .Setup(r => r.ReadChatMinuteRecords(
+                    It.IsAny<PositiveInt>(), It.IsAny<PositiveInt>(), It.IsAny<UtcDateTime>(), It.IsAny<UtcDateTime>(), It.IsAny<CancellationToken>()))
+                .ReturnsAsync(new List<ChatMinuteRecord>());
+        else
+            RepositoryMock
+                .Setup(r => r.ReadChatAggregateRecords(
+                    granularity, It.IsAny<PositiveInt>(), It.IsAny<PositiveInt>(), It.IsAny<UtcDateTime>(), It.IsAny<UtcDateTime>(), It.IsAny<CancellationToken>()))
+                .ReturnsAsync(new List<ChatAggregateRecord>());
+
+        // Act
+        var result = await Handler.Handle(validQuery);
+
+        // Assert
+        result.AsT0.Should().NotBeNull();
+
+        if (granularity is Granularity.MinuteByMinute)
+            RepositoryMock.Verify(
+                r => r.ReadChatMinuteRecords(
+                    It.IsAny<PositiveInt>(), It.IsAny<PositiveInt>(), It.IsAny<UtcDateTime>(), It.IsAny<UtcDateTime>(), It.IsAny<CancellationToken>()),
+                Times.Once);
+        else
+            RepositoryMock.Verify(
+                r => r.ReadChatAggregateRecords(
+                    granularity, It.IsAny<PositiveInt>(), It.IsAny<PositiveInt>(), It.IsAny<UtcDateTime>(), It.IsAny<UtcDateTime>(), It.IsAny<CancellationToken>()),
+                Times.Once);
+    }
+}
